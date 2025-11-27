b8cc85ff78553e2f21cb93e608c245c35ab7bee1
demch <oleks.demchuk@gmail.com>
2025-11-27 23:53:11 +0100

    first commmit

---
+++ src/main/java/org/example/Day0.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Day0 {
+
+   public static final String INPUT1 = "INPUT1";
+   public static final String INPUT2 = "INPUT2";
+
+    public String foo(String input) {
+        log.info("Running foo {}", input);
+        String result;
+        if (input.equals(INPUT1)) {
+            result = "OUTPUT1";
+        } else if (input.equals(INPUT2)) {
+            result = "OUTPUT2";
+        } else {
+            result = "OUTPUT_DEFAULT";
+        }
+        log.info("Returning {}", result);
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        Day0 day0 = new Day0();
+        day0.foo(INPUT1);
+    }
+
+}
---
+++ src/test/java/org/example/Day0Test.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day0Test {
+
+    Day0 day0 = new Day0();
+
+    @ParameterizedTest
+    @CsvSource({"INPUT1, OUTPUT1", "INPUT2, OUTPUT2", "SOMETHING_ELSE, OUTPUT_DEFAULT"})
+    void verifyFoo(String input, String expected) {
+        String result = day0.foo(input);
+        assertEquals(result, expected);
+    }
+
+
+}
